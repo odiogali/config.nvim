3ac9f02479448cfc31081e962f2accf4d46ce74e
Odi Ogali <odi.ogali1380@gmail.com>
2024-04-27 01:29:59 -0600

    harpoon bind changed

--- init.lua
+++ init.lua
@@ -176,7 +176,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>a', mark.add_file)
       vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 
-      vim.keymap.set('n', '<C-h>', function()
+      vim.keymap.set('n', '<C-b>', function()
         ui.nav_file(1)
       end)
       vim.keymap.set('n', '<C-t>', function()
