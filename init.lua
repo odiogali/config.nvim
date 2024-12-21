35497824f1aad0853d97acb7b2cca4f8ffe34e63
Odi Ogali <odi.ogali1380@gmail.com>
2024-12-21 15:27:23 -0700

    Vim navigator and various other updates

--- init.lua
+++ init.lua
@@ -160,9 +160,6 @@ require('lazy').setup({
 
   {
     'nvim-lualine/lualine.nvim',
-    opts = {
-      theme = 'gruvbox-material',
-    },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 
@@ -174,6 +171,23 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
   {
     'christoomey/vim-tmux-navigator',
     cmd = {
@@ -229,18 +243,18 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  --{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --  'lewis6991/gitsigns.nvim',
+  --  opts = {
+  --    signs = {
+  --      add = { text = '+' },
+  --      change = { text = '~' },
+  --      delete = { text = '_' },
+  --      topdelete = { text = '‾' },
+  --      changedelete = { text = '~' },
+  --    },
+  --  },
+  --},
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -755,24 +769,18 @@ require('lazy').setup({
     end,
   },
 
-  --{ -- You can easily change to a different colorscheme.
-  --  -- Change the name of the colorscheme plugin below, and then
-  --  -- change the command in the config to whatever the name of that colorscheme is.
-  --  --
-  --  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --  'rebelot/kanagawa.nvim',
-  --  priority = 1000, -- Make sure to load this before all the other start plugins.
-  --  init = function()
-  --    -- Load the colorscheme here.
-  --    -- Like many other themes, this one has different styles, and you could load
-  --    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --    vim.cmd.colorscheme 'kanagawa-wave'
-  --    -- vim.o.background = 'dark'
+  {
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'kanagawa-wave'
+      vim.o.background = 'dark'
 
-  --    -- You can configure highlights by doing something like:
-  --    vim.cmd.hi 'Comment gui=none'
-  --  end,
-  --},
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
   --{ -- You can easily change to a different colorscheme.
   --  -- Change the name of the colorscheme plugin below, and then
@@ -786,26 +794,27 @@ require('lazy').setup({
   --    -- Like many other themes, this one has different styles, and you could load
   --    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
   --    vim.cmd.colorscheme 'dracula'
+  --    --vim.o.background = 'dark'
+
+  --    -- You can configure highlights by doing something like:
+  --    vim.cmd.hi 'Comment gui=none'
+  --  end,
+  --},
+
+  --{
+  --  'sainnhe/gruvbox-material',
+  --  priority = 1000, -- Make sure to load this before all the other start plugins.
+  --  init = function()
+  --    -- Load the colorscheme here.
+  --    -- Like many other themes, this one has different styles, and you could load
+  --    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --    vim.cmd.colorscheme 'gruvbox-material'
   --    -- vim.o.background = 'dark'
 
   --    -- You can configure highlights by doing something like:
   --    vim.cmd.hi 'Comment gui=none'
   --  end,
   --},
-  {
-    'sainnhe/gruvbox-material',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox-material'
-      -- vim.o.background = 'dark'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
