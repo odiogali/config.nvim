105cbae0d2bcab0863a9e35ff810d9428e6a4e4b
Odi Ogali <odi.ogali1380@gmail.com>
2025-01-06 13:21:29 -0700

    Background is now transparent.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,10 +4,13 @@
 return {
   'sainnhe/gruvbox-material',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
+  lazy = false,
+  config = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.g.gruvbox_material_transparent_background = 1
+    vim.g.gruvbox_material_ui_contrast = 'high' -- The contrast of line numbers, indent lines, etc.
     vim.cmd.colorscheme 'gruvbox-material'
     -- vim.o.background = 'dark'
 
