ca913b13a35fcff5933cd2961037aeddcc2a2a3a
Odi Ogali <odi.ogali1380@gmail.com>
2025-06-17 09:06:23 -0600

    Shift k should not be bound to anything tf?

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -34,9 +34,6 @@ return {
       ['<C-p>'] = { 'select_prev', 'fallback' },
       ['<C-n>'] = { 'select_next', 'fallback' },
 
-      ['<S-k>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<S-j>'] = { 'scroll_documentation_down', 'fallback' },
-
       ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
       ['<C-e>'] = { 'hide', 'fallback' },
     }
